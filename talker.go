7608c895ec73757cab7eb1b7c0a6d9d88f238426
SeanTolstoyevski <lostpars@outlook.com>
2020-11-14 21:34:11 +0300

    improves

--- talker.go
+++ talker.go
@@ -39,8 +39,7 @@ func PreferSAPI(yesno bool) {
 }
 
 func DetectScreenReader() string {
-	detect := WCharToString(C.Tolk_DetectScreenReader())
-	return detect
+	return WCharToString(C.Tolk_DetectScreenReader())
 }
 
 func Output(text string, interrupt bool) (bool, error) {
@@ -48,7 +47,43 @@ func Output(text string, interrupt bool) (bool, error) {
 	if err != nil {
 		return false, err
 		}
-	ok := C.Tolk_Output(wcharstr, C.bool(interrupt))
-	return bool(ok), nil
+	return bool(C.Tolk_Output(wcharstr, C.bool(interrupt))), nil
 }
 
+// C Description:  Tests if the current screen reader driver supports speech output, if one is set. If none is set, tries to detect the currently active screen reader before testing for speech support. You should call Tolk_Load once before using this function.
+func HasSpeech() bool {
+	return bool(C.Tolk_HasSpeech())
+}
+
+// C Description:  Tests if the current screen reader driver supports braille output, if one is set. If none is set, tries to detect the currently active screen reader before testing for braille support. You should call Tolk_Load once before using this function.
+func HasBraille() bool {
+	return bool(C.Tolk_HasBraille())
+}
+
+// C Description:  Speaks text through the current screen reader driver, if one is set and supports speech output. If none is set or if it encountered an error, tries to detect the currently active screen reader before speaking the text. Use this function only if you specifically need to speak text through the current screen reader without also brailling it. Not all screen reader drivers may support this functionality. Therefore, use Tolk_Output whenever possible. You should call Tolk_Load once before using this function. This function is asynchronous.
+func Speak(value string, interrupt bool) bool {
+	v, err := StringToWchart(value)
+	if err != nil {
+		return false
+		}
+	return bool(C.Tolk_Speak(v, C.bool(interrupt)))
+}
+
+// C Description:  Brailles text through the current screen reader driver, if one is set and supports braille output. If none is set or if it encountered an error, tries to detect the currently active screen reader before brailling the given text. Use this function only if you specifically need to braille text through the current screen reader without also speaking it. Not all screen reader drivers may support this functionality. Therefore, use Tolk_Output whenever possible. You should call Tolk_Load once before using this function.
+func Braille(value string) bool {
+	v, err := StringToWchart(value)
+	if err != nil {
+		return false
+		}
+	return bool(C.Tolk_Braille(v))
+}
+
+// C Description:  Tests if the screen reader associated with the current screen reader driver is speaking, if one is set and supports querying for status information. If none is set, tries to detect the currently active screen reader before testing if it is speaking. You should call Tolk_Load once before using this function.
+func IsSpeaking() bool {
+	return bool(C.Tolk_IsSpeaking())
+}
+
+// C Description:  Silences the screen reader associated with the current screen reader driver, if one is set and supports speech output. If none is set or if it encountered an error, tries to detect the currently active screen reader before silencing it. You should call Tolk_Load once before using this function.
+func Silence() bool {
+	return bool(C.Tolk_Silence())
+}
