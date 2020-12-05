c9ee1b29016f2fd104d9e89cd11d98de429b82da
SeanTolstoyevski <lostpars@outlook.com>
2020-12-06 02:41:42 +0300

    improve docs and remove tags.

--- link_windows_amd64.go
+++ link_windows_amd64.go
@@ -1,7 +1,5 @@
-// +build bit64
-
 package talker
 
-//#cgo windows  CFLAGS: -DGO_WINDOWS -Iinclude
+//#cgo windows  CFLAGS: -O3 -Wall -Iinclude
 //#cgo windows  LDFLAGS: -Llib/x64 -lTolk
 import "C"
--- link_windows_i386.go
+++ link_windows_i386.go
@@ -1,8 +1,5 @@
-// +build bit32
-
 package talker
 
-//#cgo windows  CFLAGS: -DGO_WINDOWS -Iinclude
+//#cgo windows  CFLAGS: -O3 -Wall -Iinclude
 //#cgo windows  LDFLAGS: -Llib/x86 -lTolk
 import "C"
-
